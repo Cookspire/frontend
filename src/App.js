b8598d09cc98726320145a5869a51f8a657884ff
kanishkar <kanishkrish123@gmail.com>
2023-10-05 20:13:34 +0100

    Minor code changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import './environment/APIService'
+import { URL } from './environment/APIService';
 
 function App() {
+
+  console.log("backend service"+URL.API_URL)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Cookspire</h1>
       </header>
     </div>
   );
