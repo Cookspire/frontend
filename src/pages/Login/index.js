ae3334b8ee2bfeb510abfd04e444ddf13db66b8d
kanishkar <kanishkrish123@gmail.com>
2023-10-07 21:52:16 +0100

    Minor UI changes

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,11 +4,44 @@ export default function Login() {
     <div className="center-modal">
       <div className="center-content">
         <div className="center-content-header">Sign in to CookSpire</div>
+
         <div className="center-content-body">
-            <div className="login-form">
+          <form className="login-form">
+            <div className="form-field">
+              <div className="field-label">
                 <label>Email</label>
-                <input type="text" placeholder="hello@example.com"/>
+              </div>
+
+              <div className="field-content">
+                <input
+                  type="text"
+                  id="email"
+                  placeholder="hello@example.com"
+                  required
+                />
+              </div>
             </div>
+
+            <div className="form-field">
+              <div className="field-label">
+                <label>Password</label>
+              </div>
+
+              <div className="field-content">
+                <input type="password" id="password" required />
+              </div>
+            </div>
+
+            <div className="form-button">
+              <div className="field-button">
+                <button type="submit">Sign In</button>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className="center-content-footer">
+          New to CookSpire? <a href="/register">Create an account</a>
         </div>
       </div>
     </div>
