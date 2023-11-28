da8906487adf876a2172639fb321f3800824f087
kanishkar <kanishkrish123@gmail.com>
2023-11-28 13:44:40 +0000

    Minor changes

--- src/components/ui/RecipeDetails.js
+++ src/components/ui/RecipeDetails.js
@@ -4,6 +4,7 @@ import CloseModal from "../../hooks/CloseModal";
 import "../styles/RecipeDetails.css";
 import { UpdateNotificationContext } from "../../context/NotificationContext";
 import { NotificationType } from "../../environment/APIService";
+import Notification from "./Notification";
 
 const OVERLAY_STYLE = {
   backgroundColor: "rgb(0 0 0 / 70%)",
@@ -21,9 +22,7 @@ const OVERLAY_STYLE = {
 };
 
 export default function RecipeDetails({ handleClose, createRecipe }) {
-
   const setNotificationData = useContext(UpdateNotificationContext);
-
 
   const outsideClick = CloseModal(() => {
     handleClose(false);
@@ -52,24 +51,20 @@ export default function RecipeDetails({ handleClose, createRecipe }) {
         file.type === "image/png" ||
         file.type === "image/jpg"
       ) {
-
-        if(file.size<=50000){
+        if (file.size <= 50000) {
           setFilePreview((prev) => ({
             ...prev,
             show: true,
             imageURL: URL.createObjectURL(uploadPostImageRef.current.files[0]),
           }));
-        }
-        else{
+        } else {
           setNotificationData(
             true,
             "Kindly upload image size less than 50KB.",
             NotificationType.INFO
           );
         }
-       
-      }
-      else{
+      } else {
         setNotificationData(
           true,
           "Only .jpg and .png files are allowed.",
@@ -94,223 +89,226 @@ export default function RecipeDetails({ handleClose, createRecipe }) {
   });
 
   return (
-    <div style={OVERLAY_STYLE}>
-      <div className="recipe-detail-container" ref={outsideClick}>
-        {!createRecipe && (
-          <div className="recipe-dialog">
-            <div className="header">Let's Cook!</div>
+    <>
+      <Notification />
+      <div style={OVERLAY_STYLE}>
+        <div className="recipe-detail-container" ref={outsideClick}>
+          {!createRecipe && (
+            <div className="recipe-dialog">
+              <div className="header">Let's Cook!</div>
 
-            <div className="image">
-              <img
-                alt="coffee"
-                src="https://images.unsplash.com/photo-1497636577773-f1231844b336?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              ></img>
-            </div>
+              <div className="image">
+                <img
+                  alt="coffee"
+                  src="https://images.unsplash.com/photo-1497636577773-f1231844b336?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                ></img>
+              </div>
 
-            <div className="name">Coffee</div>
+              <div className="name">Coffee</div>
 
-            <div className="duration">Total time: 20min</div>
+              <div className="duration">Total time: 20min</div>
 
-            <div className="actions">
-              <nav className="menu">
-                <div className="actions-header">
-                  <div className="header-block">
-                    <div
-                      className="details-block active-block"
-                      ref={ingredientsBlock}
-                      onClick={() => {
-                        ingredientsBlock.current.className =
-                          "details-block active-block";
-                        instructionsBlock.current.className = "details-block";
-                        setShowInstructions(false);
-                      }}
-                    >
-                      Ingredients
-                    </div>
-
-                    <div
-                      className="details-block"
-                      ref={instructionsBlock}
-                      onClick={(e) => {
-                        ingredientsBlock.current.className = "details-block";
-                        instructionsBlock.current.className =
-                          "details-block active-block";
-                        setShowInstructions(true);
-                      }}
-                    >
-                      Instructions
-                    </div>
-                  </div>
-
-                  <div className="menu-control">
-                    {!showInstructions && (
-                      <div className="menu-body">
-                        <div className="menu-table">
-                          <table>
-                            <tr>
-                              <th>Item</th>
-                              <th>Quantity</th>
-                            </tr>
-                            <tr>
-                              <td>Milk</td>
-                              <td>1 ltr</td>
-                            </tr>
-                            <tr>
-                              <td>Coffee Powder</td>
-                              <td>1 spoon</td>
-                            </tr>
-                          </table>
-                        </div>
+              <div className="actions">
+                <nav className="menu">
+                  <div className="actions-header">
+                    <div className="header-block">
+                      <div
+                        className="details-block active-block"
+                        ref={ingredientsBlock}
+                        onClick={() => {
+                          ingredientsBlock.current.className =
+                            "details-block active-block";
+                          instructionsBlock.current.className = "details-block";
+                          setShowInstructions(false);
+                        }}
+                      >
+                        Ingredients
                       </div>
-                    )}
 
-                    {showInstructions && (
-                      <div className="menu-body">
-                        <div className="heading">Cooking Instructions</div>
-
-                        <div className="sub-heading">
-                          Follow step by step to cook the recipe perfectly!
-                        </div>
-                        <div className="steps">
-                          <ol>
-                            <li>Add milk</li>
-                            <li>Add sugar</li>
-                            <li>Mix coffee powder</li>
-                          </ol>
-                        </div>
+                      <div
+                        className="details-block"
+                        ref={instructionsBlock}
+                        onClick={(e) => {
+                          ingredientsBlock.current.className = "details-block";
+                          instructionsBlock.current.className =
+                            "details-block active-block";
+                          setShowInstructions(true);
+                        }}
+                      >
+                        Instructions
                       </div>
-                    )}
-                  </div>
-                </div>
-              </nav>
-            </div>
-          </div>
-        )}
-
-        {createRecipe && (
-          <div className="recipe-dialog">
-            <div className="header">Create Recipe</div>
-
-            <div className="image">
-              <input
-                type="file"
-                ref={uploadPostImageRef}
-                id="post-image"
-                style={{ display: "none" }}
-                accept=".png, .jpg, image/png, image/jg"
-                onChange={(e) => handleFileUpload(e)}
-              />
-
-              {filePreview.show && (
-                <>
-                  <div className="post-image-container">
-                    <img
-                      className="post-image"
-                      alt="imagePreview"
-                      src={filePreview.imageURL}
-                    />
-                    <div
-                      className="remove-pic"
-                      title="remove photo"
-                      onClick={(e) => removeImage(e)}
-                    >
-                      <div className="remove-icon">&times;</div>
-                    </div>
-                  </div>
-                </>
-              )}
-
-              {!filePreview.show && (
-                <div
-                  className="photo-attachment"
-                  onClick={(e) => openFileUpload(e)}
-                  title="Upload Image"
-                >
-                  <AddPhotoAlternateIcon htmlColor="grey" fontSize="large" />
-                </div>
-              )}
-            </div>
-
-            <div className="name">Coffee</div>
-
-            <div className="duration">Total time: 20min</div>
-
-            <div className="actions">
-              <nav className="menu">
-                <div className="actions-header">
-                  <div className="header-block">
-                    <div
-                      className="details-block active-block"
-                      ref={ingredientsBlock}
-                      onClick={() => {
-                        ingredientsBlock.current.className =
-                          "details-block active-block";
-                        instructionsBlock.current.className = "details-block";
-                        setShowInstructions(false);
-                      }}
-                    >
-                      Ingredients
                     </div>
 
-                    <div
-                      className="details-block"
-                      ref={instructionsBlock}
-                      onClick={(e) => {
-                        ingredientsBlock.current.className = "details-block";
-                        instructionsBlock.current.className =
-                          "details-block active-block";
-                        setShowInstructions(true);
-                      }}
-                    >
-                      Instructions
+                    <div className="menu-control">
+                      {!showInstructions && (
+                        <div className="menu-body">
+                          <div className="menu-table">
+                            <table>
+                              <tr>
+                                <th>Item</th>
+                                <th>Quantity</th>
+                              </tr>
+                              <tr>
+                                <td>Milk</td>
+                                <td>1 ltr</td>
+                              </tr>
+                              <tr>
+                                <td>Coffee Powder</td>
+                                <td>1 spoon</td>
+                              </tr>
+                            </table>
+                          </div>
+                        </div>
+                      )}
+
+                      {showInstructions && (
+                        <div className="menu-body">
+                          <div className="heading">Cooking Instructions</div>
+
+                          <div className="sub-heading">
+                            Follow step by step to cook the recipe perfectly!
+                          </div>
+                          <div className="steps">
+                            <ol>
+                              <li>Add milk</li>
+                              <li>Add sugar</li>
+                              <li>Mix coffee powder</li>
+                            </ol>
+                          </div>
+                        </div>
+                      )}
                     </div>
                   </div>
-
-                  <div className="menu-control">
-                    {!showInstructions && (
-                      <div className="menu-body">
-                        <div className="menu-table">
-                          <table>
-                            <tr>
-                              <th>Item</th>
-                              <th>Quantity</th>
-                            </tr>
-                            <tr>
-                              <td>Milk</td>
-                              <td>1 ltr</td>
-                            </tr>
-                            <tr>
-                              <td>Coffee Powder</td>
-                              <td>1 spoon</td>
-                            </tr>
-                          </table>
-                        </div>
-                      </div>
-                    )}
-
-                    {showInstructions && (
-                      <div className="menu-body">
-                        <div className="heading">Cooking Instructions</div>
-
-                        <div className="sub-heading">
-                          Follow step by step to cook the recipe perfectly!
-                        </div>
-                        <div className="steps">
-                          <ol>
-                            <li>Add milk</li>
-                            <li>Add sugar</li>
-                            <li>Mix coffee powder</li>
-                          </ol>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </nav>
+                </nav>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+
+          {createRecipe && (
+            <div className="recipe-dialog">
+              <div className="header">Create Recipe</div>
+
+              <div className="image">
+                <input
+                  type="file"
+                  ref={uploadPostImageRef}
+                  id="post-image"
+                  style={{ display: "none" }}
+                  accept=".png, .jpg, image/png, image/jg"
+                  onChange={(e) => handleFileUpload(e)}
+                />
+
+                {filePreview.show && (
+                  <>
+                    <div className="post-image-container">
+                      <img
+                        className="post-image"
+                        alt="imagePreview"
+                        src={filePreview.imageURL}
+                      />
+                      <div
+                        className="remove-pic"
+                        title="remove photo"
+                        onClick={(e) => removeImage(e)}
+                      >
+                        <div className="remove-icon">&times;</div>
+                      </div>
+                    </div>
+                  </>
+                )}
+
+                {!filePreview.show && (
+                  <div
+                    className="photo-attachment"
+                    onClick={(e) => openFileUpload(e)}
+                    title="Upload Image"
+                  >
+                    <AddPhotoAlternateIcon htmlColor="grey" fontSize="large" />
+                  </div>
+                )}
+              </div>
+
+              <div className="name">Coffee</div>
+
+              <div className="duration">Total time: 20min</div>
+
+              <div className="actions">
+                <nav className="menu">
+                  <div className="actions-header">
+                    <div className="header-block">
+                      <div
+                        className="details-block active-block"
+                        ref={ingredientsBlock}
+                        onClick={() => {
+                          ingredientsBlock.current.className =
+                            "details-block active-block";
+                          instructionsBlock.current.className = "details-block";
+                          setShowInstructions(false);
+                        }}
+                      >
+                        Ingredients
+                      </div>
+
+                      <div
+                        className="details-block"
+                        ref={instructionsBlock}
+                        onClick={(e) => {
+                          ingredientsBlock.current.className = "details-block";
+                          instructionsBlock.current.className =
+                            "details-block active-block";
+                          setShowInstructions(true);
+                        }}
+                      >
+                        Instructions
+                      </div>
+                    </div>
+
+                    <div className="menu-control">
+                      {!showInstructions && (
+                        <div className="menu-body">
+                          <div className="menu-table">
+                            <table>
+                              <tr>
+                                <th>Item</th>
+                                <th>Quantity</th>
+                              </tr>
+                              <tr>
+                                <td>Milk</td>
+                                <td>1 ltr</td>
+                              </tr>
+                              <tr>
+                                <td>Coffee Powder</td>
+                                <td>1 spoon</td>
+                              </tr>
+                            </table>
+                          </div>
+                        </div>
+                      )}
+
+                      {showInstructions && (
+                        <div className="menu-body">
+                          <div className="heading">Cooking Instructions</div>
+
+                          <div className="sub-heading">
+                            Follow step by step to cook the recipe perfectly!
+                          </div>
+                          <div className="steps">
+                            <ol>
+                              <li>Add milk</li>
+                              <li>Add sugar</li>
+                              <li>Mix coffee powder</li>
+                            </ol>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
